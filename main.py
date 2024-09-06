5a094b72c9e6a887d45e68fef5d656755eee0cf8
Saad-J21 <saaddeveloper21@gmail.com>
2024-09-07 02:53:51 +0300

    B: finished game logic

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
   pygame.init()
+  clock = pygame.time.Clock()
+  dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
+
+  Player.containers = (updatable, drawable)
+  Asteroid.containers = (asteroids, updatable, drawable)
+  AsteroidField.containers = (updatable)
+  Shot.containers = (shots, updatable, drawable)
+
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  asteroid_field = AsteroidField()
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+
     screen.fill((0, 0, 0))
+
+    for obj in drawable:
+      obj.draw(screen)
+
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
+    
+    for obj in updatable:
+      obj.update(dt)
+
+    for asteroid in asteroids:
+      if player.collides_with(asteroid):
+        print("Game Over!")
+        sys.exit()
+      for shot in shots:
+        if shot.collides_with(asteroid):
+          asteroid.split()
+          shot.kill()
+
+
+
   # print("Starting asteroids!")
   # print(f"Screen width: {SCREEN_WIDTH}")
   # print(f"Screen height: {SCREEN_HEIGHT}")
